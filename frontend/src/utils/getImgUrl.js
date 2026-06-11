532af867876988d5ac1bd995252fd6c54d03ea80
Rupa Maurya <rupamaurya334@gmail.com>
2026-06-11 11:09:53 +0530

    fix baseURL to use environment variable

--- frontend/src/utils/getImgUrl.js
+++ frontend/src/utils/getImgUrl.js
@@ -1,15 +1,9 @@
 function getImgUrl(name) {
-    // If it's already a full URL (http/https), return as-is
     if (!name) return '';
     if (name.startsWith('http://') || name.startsWith('https://')) {
         return name;
     }
-    // Otherwise treat as local asset filename
-    try {
-        return new URL(`../assets/books/${name}`, import.meta.url).href;
-    } catch {
-        return '';
-    }
+    return new URL(`../assets/books/${name}`, import.meta.url).href;
 }
 
 export { getImgUrl }
